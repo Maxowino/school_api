be444b125cb652ed70c57ab9da251406d4360e18
Maxowino <maxwellowino27@gmail.com>
2023-11-28 17:36:42 +0300

    api methods added

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/create-course',[CourseController::class,'createCourse']);
+Route::get('/get-courses',[CourseController::class,'readAllCourses']);
+Route::get('/get-course/{id}',[CourseController::class,'readCourse']);
