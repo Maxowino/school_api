828b3cf5804269166d9fbb91b628f89eefe257b5
Maxowino <maxwellowino27@gmail.com>
2023-12-02 11:54:44 +0300

    api-change

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/create-course',[CourseController::class,'createCourse']);
 Route::get('/get-courses',[CourseController::class,'readAllCourses']);
 Route::get('/get-course/{id}',[CourseController::class,'readCourse']);
+Route::put('/updateCourse/{id}',[CourseController::class,'updateCourse']);
+Route::delete('/deleteCourse/{id}',[CourseController::class,'deleteCourse']);
